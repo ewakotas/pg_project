1f822b544fd96d569b9ecfed4f7f649214d10afb
miki90014 <tinusia09@gmail.com>
2023-06-13 19:15:14 +0200

    Add hierarchii of scenes

---
+++ 2DplatformGame/Assets/Scripts/go_to_next_scene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class go_to_next_scene : MonoBehaviour
+{
+    public float timeRemaining = 56;
+    public bool timerIsRunning = false;
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+
+    }
+}
